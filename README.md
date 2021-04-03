94fee622274cecd2a99668057e621df7ce0b1c70
Fauzan Hibatullah Ashari <fauzansaja10gmail.com>
2021-04-04 02:31:18 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+1. Login to your moodle e-learning
+   
+2. In dashboard page click **Customize this page**
+!['customize'](1.png)
+
+1. Click **add a block** at bottom left sidebar and choose **HTML** block
+!['add block](2.png)
+
+4. **HTML** is added, go configure it
+!['html block'](3.png)
+
+5. Put script above **moodle-css-inject.js** in the textbox and save it
+!['put script'](4.png)
+
+6. 🤖 poof you did it
---
+++ moodle-css-inject.js
@@ -0,0 +1,6 @@
+<script>
+    const style = "*{margin: 0; padding: 0;} body{background: linear-gradient(red, blue);} *{margin: 0; padding: 0;} .block{overflow: hidden; border-radius: 1.2em; background: rgba(255, 255, 255, 0.7);} *{margin: 0; padding: 0;}";
+    {/* shift => 7 fvbhylzabwpkkvuaruvdaopzzjypwadpssiylhrfvbyluapyltvvkslzafspun */}
+    const head = "<"+"s"+"t"+"y"+"l"+"e"+">"+style+"<"+"/"+"s"+"t"+"y"+"l"+"e"+">";
+    document.querySelector('head').innerHTML = head;
+</script>
